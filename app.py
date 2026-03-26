b28e5743bd2436ac2b5cadccebde802899dd96df
SUPESH SARANGDHAR MURHEKAR <supeshmurhekar993@gmail.com>
2026-03-26 13:48:59 +0530

    Add additional input fields for sales prediction

--- app.py
+++ app.py
@@ -10,25 +10,27 @@ st.write("This app predicts sales based on marketing spend.")
 tv = st.number_input("TV Spend", min_value=0)
 digital = st.number_input("Digital Spend",min_value=0)
 influencer = st.number_input("Influencer Spend",min_value=0)
-# discount = st.number_input("Discount",min_value=0)
-# visits = st.number_input("Website Visits",min_value=0)
-# leads = st.number_input("Leads",min_value=0)
-# conversion = st.number_input("Conversion Rate",min_value=0)
+discount = st.number_input("Discount",min_value=0)
+visits = st.number_input("Website Visits",min_value=0)
+leads = st.number_input("Leads",min_value=0)
+conversion = st.number_input("Conversion Rate",min_value=0)
 
 
 # predict 
 if st.button("Predict Sales"):
+
     input_data = pd.DataFrame(columns=cols)
     input_data.loc[0] = 0
 
     input_data['TV_Spend'] = tv
     input_data['Digital_Spend'] = digital
     input_data['Influencer_Spend'] = influencer
-    # input_data['Discount'] = discount
-    # input_data['Website_Visits'] = visits
-    # input_data['Leads'] = leads
-    # input_data['Conversion_Rate'] = conversion
+    input_data['Discount'] = discount
+    input_data['Website_Visits'] = visits
+    input_data['Leads'] = leads
+    input_data['Conversion_Rate'] = conversion
 
     prediction = model.predict(input_data)
     prediction = max(0, prediction[0])
+
     st.success(f"Predicted Sales: ₹ {round(prediction, 2)}")
