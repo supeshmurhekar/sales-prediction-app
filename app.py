135b1c73c766db6a60d9c3a0bde146f2bc8c7511
SUPESH SARANGDHAR MURHEKAR <supeshmurhekar993@gmail.com>
2026-03-26 13:45:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ if st.button("Predict Sales"):
     # input_data['Leads'] = leads
     # input_data['Conversion_Rate'] = conversion
 
-    prediction = model.predict(input_data)
-
-    st.success(f"Predicted Sales: ₹ {round(prediction[0],2)}")
+   prediction = model.predict(input_data)
+   prediction = max(0, prediction[0])
+   st.success(f"Predicted Sales: ₹ {round(prediction[0],2)}")
