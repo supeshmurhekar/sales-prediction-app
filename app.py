24889b136e4c3f323b6eed0e4aad043760ce3397
SUPESH SARANGDHAR MURHEKAR <supeshmurhekar993@gmail.com>
2026-03-26 13:18:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import pickle
+# load model and columns 
+model  = pickle.load(open("D:\Data Science\linear regression\sales_model.pkl","rb"))
+cols = pickle.load(open("D:\Data Science\linear regression\columns.pkl","rb"))
+st.title("Sales Predication App")
+st.write("This app predicts sales based on marketing spend.")
+# inputs
+tv = st.number_input("TV Spend", min_value=0)
+digital = st.number_input("Digital Spend",min_value=0)
+influencer = st.number_input("Influencer Spend",min_value=0)
+discount = st.number_input("Discount",min_value=0)
+visits = st.number_input("Website Visits",min_value=0)
+leads = st.number_input("Leads",min_value=0)
+conversion = st.number_input("Conversion Rate",min_value=0)
+
+
+# predict 
+if st.button("Predict Sales"):
+    input_data = pd.DataFrame(columns=cols)
+    input_data.loc[0] = 0
+
+    input_data['TV_Spend'] = tv
+    input_data['Digital_Spend'] = digital
+    input_data['Influencer_Spend'] = influencer
+    input_data['Discount'] = discount
+    input_data['Website_Visits'] = visits
+    input_data['Leads'] = leads
+    input_data['Conversion_Rate'] = conversion
+
+    prediction = model.predict(input_data)
+
+    st.success(f"Predicted Sales: ₹ {round(prediction[0],2)}")
